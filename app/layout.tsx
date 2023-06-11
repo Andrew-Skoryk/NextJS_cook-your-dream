c03f8ab1dedba825973632bedcb95a58d9ec8042
Andrew Skoryk <andrewtimelessone@gmail.com>
2023-06-11 16:13:18 +0300

    fix: deleted leftovers from next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import "./globals.css";
 import { Lora } from "next/font/google";
-import { Providers } from "./providers";
 import { ClerkProvider } from "@clerk/nextjs/app-beta";
 
 export const metadata = {
@@ -24,15 +23,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className={lora.className}>
         <body className="flex min-h-screen flex-col bg-yellow-100 text-green-900">
-          <Providers>
-            <Navigation />
+          <Navigation />
 
-            <main className="m-auto max-w-screen-2xl flex-grow px-12">
-              {children}
-            </main>
+          <main className="m-auto max-w-screen-2xl flex-grow px-12">
+            {children}
+          </main>
 
-            <Footer />
-          </Providers>
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
